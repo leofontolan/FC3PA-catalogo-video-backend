48ad002fa9d6d34ad06eaa4cac5a246eacbbb659
leofontolan <leofontolan@gmail.com>
2022-03-09 18:28:53 -0300

    ajuste do teste do construtor

--- src/category/domain/entities/category.spec.ts
+++ src/category/domain/entities/category.spec.ts
@@ -5,22 +5,32 @@ describe("Category Unit Tests", () => {
 
   test("constructor of category", () => {
     
+
+
+    //Teste passando apenas Nome
     let category = new Category({name: 'Movie'});
     let props = omit(category.props, 'created_at');
-    expect( props).toStrictEqual({
+    expect(props).toStrictEqual({
       name: "Movie",
       description: null,
       is_active: true,
     });
+
+    //Verificar se a variável tem um instacia de Date
     expect(category.props.created_at).toBeInstanceOf(Date);
 
 
 
+
+
+
+    //Teste passando  Nome, Descrição, is_active e Data!
     category = new Category({
       name: 'Movie', 
       description: 'some description', 
       is_active: false,
     })
+    //Criar uma Data para o teste
     let created_at = new Date();
     expect( category.props).toStrictEqual({
       name: "Movie",
@@ -32,18 +42,23 @@ describe("Category Unit Tests", () => {
 
 
 
+
+
+    // Passando apenas Nome e Descrição
     category = new Category({
       name: 'Movie', 
       description: 'other description', 
     })
-    expect( category.props).toMatchObject({
+    expect(category.props).toMatchObject({
       name: "Movie",
       description: 'other description',
     });
 
 
 
-    
+
+
+    // Passando apenas Nome e is_active
     category = new Category({
       name: 'Movie', 
       is_active: true, 
@@ -54,6 +69,10 @@ describe("Category Unit Tests", () => {
     });
 
 
+
+    
+
+    // Passando apenas Nome e created_at
     created_at = new Date();
     category = new Category({
       name: 'Movie', 
