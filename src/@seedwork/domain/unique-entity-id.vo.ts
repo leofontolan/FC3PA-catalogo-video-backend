073d4c8657ee567585bece78c22c3234d8be4b87
leofontolan <leofontolan@gmail.com>
2022-03-29 22:22:31 -0300

    Refatorando a Ent. Categoria, impelentando O.V. ID

--- src/@seedwork/domain/unique-entity-id.vo.ts
+++ src/@seedwork/domain/unique-entity-id.vo.ts
@@ -1,7 +1,8 @@
 import {v4 as uuidv4, validate as uuiValidade} from 'uuid'
+import InvalidUUIDError from '../errors/invalid-uuid.error';
+
 
 export default class UniqueEntityId {
-
 
     constructor(public readonly id?: string) {
        this.id = id || uuidv4();
@@ -9,7 +10,9 @@ export default class UniqueEntityId {
 
     private validade(){
         const isValid = uuiValidade(this.id);
-    }
-    
-}
 
+        if(!isValid){
+            throw new InvalidUUIDError(this.id);
+        }
+    }
+}
---
+++ src/@seedwork/errors/invalid-uuid.error.ts
@@ -0,0 +1,8 @@
+export default class InvalidUUIDError extends Error {
+
+
+    constructor(message?: string) {
+        super(message || 'ID must be a valid UUID');
+        this.name = "InvalidUUIDError";
+    }
+}
