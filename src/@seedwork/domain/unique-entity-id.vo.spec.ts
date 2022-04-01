ae456b84d63b3a9b52c27f53b5918b0c35e506ea
leofontolan <leofontolan@gmail.com>
2022-03-31 22:56:51 -0300

    Entendendo ClearMocks

--- src/@seedwork/domain/unique-entity-id.vo.spec.ts
+++ src/@seedwork/domain/unique-entity-id.vo.spec.ts
@@ -2,16 +2,17 @@ import InvalidUUIDError from '../errors/invalid-uuid.error';
 import UniqueEntityId from './unique-entity-id.vo';
 import { validate as uuidValidade} from 'uuid'
 
+
 describe('UniqueEntityId Unit Tests', () => {
 
+    const validadeSpy = jest.spyOn(UniqueEntityId.prototype as any, 'validade');
+
     it('should throw error when UUID is invalid', () => {
-        const validadeSpy =  jest.spyOn(UniqueEntityId.prototype as any, 'validade');
         expect(() => new UniqueEntityId("fake id")).toThrow(new InvalidUUIDError());
         expect(validadeSpy).toHaveBeenCalled();
     });
 
     it('should accept a uuid passed in constructor', () => {
-        const validadeSpy =  jest.spyOn(UniqueEntityId.prototype as any, 'validade');
         const uuid = 'a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11';
         const vo = new UniqueEntityId(uuid);
         expect(vo.id).toBe(uuid);
@@ -20,7 +21,6 @@ describe('UniqueEntityId Unit Tests', () => {
     });
 
     it('should accept a uuid passed in constructor', () => {
-        const validadeSpy =  jest.spyOn(UniqueEntityId.prototype as any, 'validade');
         const vo = new UniqueEntityId();
         expect(uuidValidade(vo.id)).toBeTruthy();
         expect(validadeSpy).toHaveBeenCalled();
